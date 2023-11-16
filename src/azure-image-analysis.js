f9dc00615d0a75fe41907a9f9875371b44a1ade8
Iago Chao <iago.chao@einsa.com>
2023-11-16 14:48:49 +0100

    Cambio key analizador

--- src/azure-image-analysis.js
+++ src/azure-image-analysis.js
@@ -6,7 +6,7 @@ use API of image analysis 4.0 from azure cognitive services.
 
 const analyzeImage = async (url) => {
     const endpoint = "https://westcentralus.api.cognitive.microsoft.com/vision/v2.0/analyze?visualFeatures=Categories,Description,Color";
-    const key = "301ce43cb0024d0f818b5ea3924ad109";
+    const key = "195c4b93-80fc-4792-9ebd-382ea9cdc0d5";
     const params = {
         method: "POST",
         headers: {
