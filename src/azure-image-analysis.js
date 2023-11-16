9fa8dd692d0aac397771f89e6a53a46aa61326e5
Iago Chao <iago.chao@einsa.com>
2023-11-16 14:23:37 +0100

    Nuevo método analisis imagen

---
+++ src/azure-image-analysis.js
@@ -0,0 +1,24 @@
+/* 
+create a function called analyzeImage that receives a url like input and returns a json with the result of the analysis.
+use API of image analysis 4.0 from azure cognitive services.
+ also can customize the visual features to be returned. 
+ */
+
+const analyzeImage = async (url) => {
+    const endpoint = "https://westcentralus.api.cognitive.microsoft.com/vision/v2.0/analyze?visualFeatures=Categories,Description,Color";
+    const key = "keyapi";
+    const params = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Ocp-Apim-Subscription-Key": key
+        },
+        body: JSON.stringify({ url })
+    };
+    const response = await fetch(endpoint, params);
+    const result = await response.json();
+    return result;
+};
+
+
+ 
