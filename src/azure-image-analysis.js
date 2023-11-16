13de8bfe0dd618fe4e3140ec5f59810c07acf096
Iago Chao <iago.chao@einsa.com>
2023-11-16 15:05:14 +0100

    Se quitan las caracteristicas

--- src/azure-image-analysis.js
+++ src/azure-image-analysis.js
@@ -10,7 +10,7 @@ const analyzeImage = async (url) => {
     // y en el ejemplo de la documentación aparece https://westcentralus.api.cognitive.microsoft.com/vision/v2.0/analyze?visualFeatures=Categories,Description,Color
     // por lo que he decidido usar el del portal de Azure.
 
-    const endpoint = "https://computer-vision-iago-ai.cognitiveservices.azure.com/vision/v4.0/analyze?visualFeatures=Categories,Description,Color";
+    const endpoint = "https://computer-vision-iago-ai.cognitiveservices.azure.com/vision/v4.0/analyze";
     const key = "0a0a05e7da0a4e41b9db8696b0c70966";
     const params = {
         method: "POST",
